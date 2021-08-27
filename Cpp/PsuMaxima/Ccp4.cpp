f3e75b3a6d237a73219eccd95593b9fc6936eda4
Rachel A <RachelAlcraft@gmail.com>
2021-08-27 14:41:15 +0100

    Added reading of binary test

--- Cpp/PsuMaxima/Ccp4.cpp
+++ Cpp/PsuMaxima/Ccp4.cpp
@@ -1,11 +1,26 @@
 #include "Ccp4.h"
 
-Ccp4::Ccp4()
+using namespace std;
+
+Ccp4::Ccp4(string ccp4, string diff)
 {
 	_resolution = 0.8;
+	//Attempt to load a binary file
+	char buffer[100];
+	ifstream myFile(ccp4, ios::in | ios::binary);
+	myFile.read(buffer, 100);
+	if (!myFile)
+		_loaded = false;
+	else
+		_loaded = true;
 }
 
 double Ccp4::Resolution()
 {
 	return _resolution;
+}
+
+bool Ccp4::Loaded()
+{
+	return _loaded;
 }
--- Cpp/PsuMaxima/Ccp4.h
+++ Cpp/PsuMaxima/Ccp4.h
@@ -3,13 +3,17 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <fstream>
 
+using namespace std;
 
 class Ccp4
 {
 private:
 	double _resolution = 0;	
+	bool _loaded = false;
 public:
-	Ccp4();
+	Ccp4(string ccp4, string diff);
 	double Resolution();
+	bool Loaded();
 };
