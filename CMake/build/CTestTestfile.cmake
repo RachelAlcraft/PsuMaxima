9b44a82449b89dfc228fb1cb8cc44dbc31b1052f
Rachel A <RachelAlcraft@gmail.com>
2021-09-03 18:14:30 +0100

    Cleaning to make it multi operating system

--- CMake/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rachel/Documents/Bioinformatics/PsuMaxima/CMake
-# Build directory: /home/rachel/Documents/Bioinformatics/PsuMaxima/CMake/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Cpp/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rachel/Documents/Bioinformatics/PsuMaxima/Cpp
-# Build directory: /home/rachel/Documents/Bioinformatics/PsuMaxima/Cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
