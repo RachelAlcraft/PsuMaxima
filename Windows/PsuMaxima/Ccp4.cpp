04ea2b4036893ab319089b62ef65fd554c884e79
Rachel A <RachelAlcraft@gmail.com>
2021-09-04 13:37:14 +0100

    File read windows

--- Windows/PsuMaxima/Ccp4.cpp
+++ Windows/PsuMaxima/Ccp4.cpp
@@ -2,16 +2,27 @@
 /************************************************************************
 * RSA 4.9.21
 ************************************************************************/
+#include<iostream>
+#include<fstream>
 
 #include "Ccp4.h"
 using namespace std;
 
 
-Ccp4::Ccp4(string pdbCode)
+Ccp4::Ccp4(string pdbCode, string directory)
 {
 	_pdbCode = pdbCode;
 	_loaded = true;
 	_resolution = 0.78;
+	_directory = directory;
+	//Load the binary data
+	ifstream infile;
+	infile.open(_directory + pdbCode + ".ccp4", ios::binary | ios::in);
+	int buffer[1024];
+	while (infile.read((char*)&buffer, sizeof(buffer)))
+	{
+		cout << (char*)&buffer;
+	}
 }
 double Ccp4::getResolution()
 {
--- Windows/PsuMaxima/Ccp4.h
+++ Windows/PsuMaxima/Ccp4.h
@@ -13,9 +13,11 @@ private:
 	bool _loaded = false;
 	string _pdbCode = "";
 	double _resolution = 0.0;
+	//SETINGS
+	string _directory = "";
 
 public:
-	Ccp4(string pdbCode);
+	Ccp4(string pdbCode, string directory);
 	double getResolution();
 	bool isLoaded();
 	string getPdbCode();
